e96ce27c081cef3acd259b62a25c882ef14307c9
ViniciusPii <vinysales@hotmail.com>
2020-11-14 14:32:24 -0300

    feat(tela): Criação da home

---
+++ lib/src/components/user_tile.dart
@@ -0,0 +1,40 @@
+import 'package:crud/src/models/user.dart';
+import 'package:flutter/material.dart';
+
+class UserTile extends StatelessWidget {
+  final User user;
+
+  const UserTile({Key key, this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final avatar = user.avatarUrl == null || user.avatarUrl.isEmpty
+        ? CircleAvatar(child: Icon(Icons.person))
+        : CircleAvatar(
+            backgroundImage: NetworkImage(user.avatarUrl),
+          );
+
+    return ListTile(
+      leading: avatar,
+      title: Text(user.name),
+      subtitle: Text(user.email),
+      trailing: Container(
+        width: 100,
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {},
+              color: Colors.orange,
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {},
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,8 +1,24 @@
+import 'package:crud/src/components/user_tile.dart';
+import 'package:crud/src/data/dummy_users.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final users = {...DUMMY_USERS};
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Usuários'),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (ctx, i) => UserTile(user: users.values.elementAt(i)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
   }
 }
