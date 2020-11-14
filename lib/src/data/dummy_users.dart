da908c4d002c233f0fe43d81011e2d425de241f7
ViniciusPii <vinysales@hotmail.com>
2020-11-14 14:06:00 -0300

    feat(data/models): Criação dos models e data

---
+++ lib/src/data/dummy_users.dart
@@ -0,0 +1,25 @@
+import 'package:crud/src/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@maria.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2018/04/09/19/55/low-poly-3305284_960_720.jpg',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@rafael.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2018/04/11/11/54/small-poly-3310319_960_720.jpg',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda@fernanda.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2018/04/06/13/46/poly-3295856_960_720.png',
+  ),
+};
